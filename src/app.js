56cb08b5220e31535728e7cb97b6191a04bce7c9
praneeth-pixzarloop <praneeth.pixzarloop@gmail.com>
2024-03-02 13:17:59 +0530

    part of praneeth

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log("im PRANEETH");
+const myModule = require('./my-module.js'); 
+console.log(myModule.myFunction()); 
+
+const myPromise = new Promise((resolve, reject) => { 
+    if (condition) { 
+      resolve('Success!'); 
+    } else { 
+      reject('Failure!'); 
+    } 
+  }); 
+   
+  myPromise.then((result) => { 
+    console.log(result); 
+  }).catch((error) => { 
+    console.log(error); 
+  }); 
+
+  async function myFunction() { 
+    try { 
+      const result = await myPromise; 
+      console.log(result); 
+    } catch (error) { 
+      console.log(error); 
+    } 
+  } 
+   
+  myFunction();
